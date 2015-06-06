15013f67c6c2fcbdbea07a0e296b545a33ef7a9f
Carlos Carrera <crlc@me.com>
2015-06-06 13:16:09 -0700

    Check-in underscore and backbone. Add SiteController, root route

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,11 @@
 BackboneDemo2::Application.routes.draw do
+  root :to => "site#root"
+
   namespace :api do
     resources :todos, only: [:create, :index, :show] do
       resources :comments, only: [:index]
     end
-    
+
     resources :comments, only: [:create, :show]
   end
 end
