0fe5010d4dda0b911e4c2f27dc763a25a1b05519
Carlos Carrera <crlc@me.com>
2015-06-06 16:32:45 -0700

    Add a refresh button to TodosIndex

--- app/assets/javascripts/views/todos_index.js
+++ app/assets/javascripts/views/todos_index.js
@@ -1,6 +1,18 @@
 window.Todo.Views.TodosIndex = Backbone.View.extend({
   template: JST["todos/index"],
 
+  events: {
+    "click button.refresh": "refresh"
+  },
+
+  refresh: function () {
+    var view = this;
+    
+    this.collection.fetch({
+      success: function () { view.render(); }
+    });
+  },
+
   render: function () {
     var renderedContent = this.template({
       todos: this.collection
